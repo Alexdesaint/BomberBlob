0686c5280df2cb0f0ddab17281d6294d3fc22029
Alexdesaint <alexandredlsb@gmail.com>
2018-09-25 07:14:16 +0200

    up

--- include/BomberBlob/InfoBar.hpp
+++ include/BomberBlob/InfoBar.hpp
@@ -1,7 +1,11 @@
 #ifndef BOMBERMAN_INFOBAR_HPP
 #define BOMBERMAN_INFOBAR_HPP
 
+#include <array>
+
 #include <SFML/Graphics.hpp>
+
+#include <BomberBlob/Player.hpp>
 
 class InfoBar {
 private:
@@ -12,10 +16,14 @@ private:
 	sf::Texture ExtraLife;
 	sf::Texture ExtraBomb;
 
+	std::array<Player*, 4> players;
+
 	sf::Text createText(const sf::String &str, unsigned int size, int x , int y);
 
 public:
 	InfoBar();
+
+	void setPlayer(unsigned int num, Player* player);
 
 	void draw(sf::RenderWindow *window);
 };
--- src/BomberBlob/InfoBar.cpp
+++ src/BomberBlob/InfoBar.cpp
@@ -11,6 +11,16 @@ sf::Text InfoBar::createText(const sf::String &str, unsigned int size, int x, in
 	text.setOrigin(textRect.left + textRect.width/2.0f, textRect.top  + textRect.height/2.0f);
 	text.setPosition(x, y);
 
+	return text;
+}
+
+InfoBar::InfoBar() {
+	font.loadFromFile("../Data/FFFFORWA.TTF");
+
+	shape.setSize(sf::Vector2f(100, 620));
+	shape.setPosition(sf::Vector2f(620, 0));
+	shape.setFillColor(sf::Color(30, 30, 30));
+
 	Image image;
 	image.loadFromFile("../data/ExtraPower.bmp");
 	image.createMaskFromColor(sf::Color(0, 255, 0), 0);
@@ -23,16 +33,11 @@ sf::Text InfoBar::createText(const sf::String &str, unsigned int size, int x, in
 	image.loadFromFile("../data/ExtraBomb.bmp");
 	image.createMaskFromColor(sf::Color(0, 255, 0), 0);
 	ExtraBomb.loadFromImage(image);
-
-	return text;
 }
 
-InfoBar::InfoBar() {
-	font.loadFromFile("../Data/FFFFORWA.TTF");
-
-	shape.setSize(sf::Vector2f(100, 620));
-	shape.setPosition(sf::Vector2f(620, 0));
-	shape.setFillColor(sf::Color(30, 30, 30));
+void InfoBar::setPlayer(unsigned int num, Player *player) {
+	if(num <= players.size())
+		players[num - 1] = player;
 }
 
 void InfoBar::draw(sf::RenderWindow *window) {
@@ -48,56 +53,25 @@ void InfoBar::draw(sf::RenderWindow *window) {
 	rec.setOrigin(8,8);
 	rec.setTexture(&ExtraLife);
 
-	rec.setPosition(650, 40);
-	window->draw(rec);
-	window->draw(createText("1", 12, 690, 40));
-
-	rec.setPosition(650, 40 + 155);
-	window->draw(rec);
-	window->draw(createText("0", 12, 690, 40 + 155));
-
-	rec.setPosition(650, 40 + 155 * 2);
-	window->draw(rec);
-	window->draw(createText("0", 12, 690, 40 + 155 * 2));
-
-	rec.setPosition(650, 40 + 155 * 3);
-	window->draw(rec);
-	window->draw(createText("0", 12, 690, 40 + 155 * 3));
-
+	for(int i = 0; i < players.size(); i++) {
+		rec.setPosition(650, 40 + i * 155);
+		window->draw(rec);
+		window->draw(createText("0", 12, 690, 40 + i * 155));
+	}
 
 	rec.setTexture(&ExtraBomb);
 
-	rec.setPosition(650, 70);
-	window->draw(rec);
-	window->draw(createText("0", 12, 690, 70));
-
-	rec.setPosition(650, 70 + 155);
-	window->draw(rec);
-	window->draw(createText("0", 12, 690, 70 + 155));
-
-	rec.setPosition(650, 70 + 155 * 2);
-	window->draw(rec);
-	window->draw(createText("0", 12, 690, 70 + 155 * 2));
-
-	rec.setPosition(650, 70 + 155 * 3);
-	window->draw(rec);
-	window->draw(createText("0", 12, 690, 70 + 155 * 3));
+	for(int i = 0; i < players.size(); i++) {
+		rec.setPosition(650, 70 + i * 155);
+		window->draw(rec);
+		window->draw(createText("0", 12, 690, 70 + i * 155));
+	}
 
 	rec.setTexture(&ExtraPower);
 
-	rec.setPosition(650, 100);
-	window->draw(rec);
-	window->draw(createText("0", 12, 690, 100));
-
-	rec.setPosition(650, 100 + 155);
-	window->draw(rec);
-	window->draw(createText("0", 12, 690, 100 + 155));
-
-	rec.setPosition(650, 100 + 155 * 2);
-	window->draw(rec);
-	window->draw(createText("0", 12, 690, 100 + 155 * 2));
-
-	rec.setPosition(650, 100 + 155 * 3);
-	window->draw(rec);
-	window->draw(createText("0", 12, 690, 100 + 155 * 3));
+	for(int i = 0; i < players.size(); i++) {
+		rec.setPosition(650, 100 + i * 155);
+		window->draw(rec);
+		window->draw(createText("0", 12, 690, 100 + i * 155));
+	}
 }
